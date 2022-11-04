c6ca16c18aeaa3853dfaf9c06069fd6aef459909
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-11-04 10:14:26 -0400

    first day

---
+++ index.js
@@ -0,0 +1,14 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
+  text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
+};
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "This is now a big red box";
+  text.style.fontSize = "40px";
+}
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
