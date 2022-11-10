4034254a5222c50a73d2516b93d87cddd9d8f0f7
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-11-10 10:13:52 -0500

    11/10/22

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/* Using JavaScript you will allow users to input information into a form. The
+form will then push data from a JavaScript object into the HTML through
+DOM manipulation. The project must contain the following
+-Create an object called “DOMSelectors” to hold your DOM Selectors
+-Create a function that creates an object and calls the following functions
+-Create a function that injects the newly created object into the DOM
+-Create a function that clears the input fields after injecting the object
+-Create a function to remove an object after they have been created */
+
+//get data from the user (get the variable save it to a user console.log it)
+//create an object from that
+//push that data into the html
+
 const DOMSelectors = {
   button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
   text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
@@ -22,9 +35,11 @@ DOMSelectors.button.addEventListener("click", function () {
 });
 
 function changeLi() {
+  let pointIndex = 1;
   DOMSelectors.points.forEach((point) => {
     point.addEventListener("click", function () {
-      point.textContent("Hello");
+      point.textContent = `Hello I am point ${pointIndex}`;
+      pointIndex++;
     });
   });
 }
