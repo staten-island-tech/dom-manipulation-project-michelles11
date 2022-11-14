be2188e3bdf3f4f65c5cd8550499079114eec775
michelles11 <113190099+michelles11@users.noreply.github.com>
2022-11-14 10:15:51 -0500

    11/14/22

--- index.js
+++ index.js
@@ -12,35 +12,13 @@ DOM manipulation. The project must contain the following
 //push that data into the html
 
 const DOMSelectors = {
-  button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
+  submit: document.querySelector("#form"), // getElementById looks for the id that is passed through it, in this case being "btn"
   text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
   box: document.getElementById("big-black-box"),
   points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
 };
 
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.innerHTML = "This is now a big red box";
-  text.style.fontSize = "40px";
-}
-
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+DOMSelectors.submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(e);
 });
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-
-function changeLi() {
-  let pointIndex = 1;
-  DOMSelectors.points.forEach((point) => {
-    point.addEventListener("click", function () {
-      point.textContent = `Hello I am point ${pointIndex}`;
-      pointIndex++;
-    });
-  });
-}
-changeLi();
